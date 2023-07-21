2101ff558693fed3a3a1be38a3ebfb7cc4d087ed
hans254 <hansfidel6@gmail.com>
2023-07-21 02:13:06 -0700

    Write a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,95 +1,88 @@
-i#include "sort.h"
-
-void swap_ints(int *a, int *b);
-int hoare_partition(int *array, size_t size, int left, int right);
-void hoare_sort(int *array, size_t size, int left, int right);
-void quick_sort_hoare(int *array, size_t size);
+#include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * _swap - swaps two values in an array
+ *
+ * @array: data to sort
+ * @i: first value
+ * @j: second value
+ *
+ * Return: No Return
  */
-void swap_ints(int *a, int *b)
+void _swap(int *array, int i, int j)
 {
 	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
 }
 
 /**
- * hoare_partition - Order a subset of an array of integers
- *                   according to the hoare partition scheme.
- * @array: The array of integers.
- * @size: The size of the array.
- * @left: The starting index of the subset to order.
- * @right: The ending index of the subset to order.
+ * partition - sorts a partition of data in relation to a pivot
  *
- * Return: The final partition index.
+ * @array: data to sort
+ * @min: Left wall
+ * @max: right wall
+ * @size: size of data
  *
- * Description: Uses the last element of the partition as the pivot.
- * Prints the array after each swap of two elements.
+ * Return: New Pivot
  */
-int hoare_partition(int *array, size_t size, int left, int right)
+int partition(int *array, int min, int max, size_t size)
 {
-	int pivot, ontop, under;
+	int a, b, pivot = array[max];
 
-	pivot = array[right];
-	for (ontop = left - 1, under = right + 1; ontop < under;)
+	for (a = min, b = max; 1; a++, b--)
 	{
-		do {
-			ontop++;
-		} while (array[ontop] < pivot);
-		do {
-			under--;
-		} while (array[under] > pivot);
+		while (array[a] < pivot)
+			a++;
 
-		if (ontop < under)
-		{
-			swap_ints(array + ontop, array + under);
-			print_array(array, size);
-		}
-	}
+		while (array[b] > pivot)
+			b--;
 
-	return (ontop);
-}
-
-/**
- * hoare_sort - Implement the quicksort algorithm through recursion.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- * @left: The starting index of the array partition to order.
- * @right: The ending index of the array partition to order.
- *
- * Description: Uses the Hoare partition scheme.
- */
-void hoare_sort(int *array, size_t size, int left, int right)
-{
-	int part;
-
-	if (right - left > 0)
-	{
-		part = hoare_partition(array, size, left, right);
-		hoare_sort(array, size, left, part - 1);
-		hoare_sort(array, size, part, right);
+		if (a >= b)
+			return (a);
+		_swap(array, a, b);
+		print_array(array, size);
 	}
 }
 
 /**
- * quick_sort_hoare - Sort an array of integers in ascending
- *                    order using the quicksort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
+ * quicksort -  sorts an array of integers in ascending order using the
+ * Quick sort algorithm Lomuto partition scheme
  *
- * Description: Uses the Hoare partition scheme. Prints
- * the array after each swap of two elements.
+ * @array: data to sort
+ * @min: Left wall
+ * @max: right wall
+ * @size: size of data
+ *
+ * Return: No Return
+ */
+void quicksort(int *array, int min, int max, size_t size)
+{
+	int r;
+
+	if (min < max)
+	{
+		r = partition(array, min, max, size);
+		quicksort(array, min, r - 1, size);
+		quicksort(array, r, max, size);
+	}
+}
+
+/**
+ * quick_sort_hoare -  sorts an array of integers in ascending order using the
+ * Quick sort algorithm Hoare partition scheme
+ *
+ * @array: data to sort
+ * @size: size of data
+ *
+ * Return: No Return
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
 
-	hoare_sort(array, size, 0, size - 1);
+	quicksort(array, 0, size - 1, size);
 }
